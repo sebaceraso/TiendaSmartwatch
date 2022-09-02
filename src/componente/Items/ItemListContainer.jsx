37cf78d5757ece1627ef38cdcadb96cbb5c34416
Sebastian Ceraso <seba.ceraso@gmail.com>
2022-09-02 00:23:48 -0300

    Modificacion del landing

--- src/componente/Items/ItemListContainer.jsx
+++ src/componente/Items/ItemListContainer.jsx
@@ -1,1 +1,13 @@
-const 
+import React from 'react'
+import "./Item.css" 
+
+export default function ItemListConteiner( { greeting } ) {
+  return (
+        <div className='greeting'>
+            <h2>
+                { greeting }
+            </h2>
+
+        </div>
+  )
+}
