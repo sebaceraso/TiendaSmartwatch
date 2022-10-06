64664dde51073d21d33da7899010062e465ae435
Sebastian Ceraso <seba.ceraso@gmail.com>
2022-10-06 14:03:52 -0300

    correcciones

--- src/componente/Items/Item.js
+++ src/componente/Items/Item.js
@@ -2,21 +2,19 @@ import { useNavigate } from 'react-router-dom'
 
 
 function Item({ item }) {
-
     const { name, id, price, description, image } = item
-
     const navegar = useNavigate()
 
     return (
         <>
-            <div className="card m-2" style={{width: '18rem'}}>
-                <img src={image} className="card-img-top" alt={name} style={{height:'15rem'}} />
-                    <div className="card-body">
-                        <h5 className="card-title">{name}</h5>
-                        <p className="card-text">{description}</p>
-                        <p className=''>$ {price}</p>
-                        <button className='btn btn-primary' onClick={()=> navegar(`/detalles/${id}`)}>Ver mas...</button>
-                    </div>
+            <div className="card m-2" style={{ width: '18rem' }}>
+                <img src={image} className="card-img-top" alt={name} style={{ height: '15rem' }} />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">{description}</p>
+                    <p className=''>$ {price}</p>
+                    <button className='btn btn-primary' onClick={() => navegar(`/detalles/${id}`)}>Ver mas...</button>
+                </div>
             </div>
         </>
     )
--- src/componente/Items/ItemListContainer.js
+++ src/componente/Items/ItemListContainer.js
@@ -1,45 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { data } from '../../mockData/MockData'
-import ItemList from './ItemList'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { data } from '../../mockData/MockData';
+import ItemList from './ItemList';
+import { getFirestore, getDocs, collection } from "firebase/firestore";
 
-export const ItemListContainer = ({ greeting, bg, pd }) => {
-    const [loading, setLoading] = useState(false)
-    const [items, setItems] = useState([])
-    const [error, setError] = useState('')
+export const ItemListContainer = () => {
+    const [items, setItems] = useState([]);
+    const { categoriaId } = useParams();
 
-    const {categoriaId} = useParams()
+    const getProducts = () => {
+        const db = getFirestore();
+        const querySnapshot = collection(db, "items");
+        getDocs(querySnapshot).then((response) => {
+            const data = response.docs.map((product) => {
+                return { id: product.id, ...product.data() };
+            });
+            setItems(data);
+        });
+    };
+
+
+    useEffect(() => {
+        getProducts();
+    }, [categoriaId]);
 
     //mock
-     useEffect(() => { 
-        data
-            .then((res) => {
-                 if(categoriaId){
-                     setItems(res.filter((item)=>item.category === categoriaId))
-                 } else {
-                     setItems(res)
-                 }
-                 })
-            .catch((err) => {
-                 setError(err)
-             })
-             .finally(() => {
-                 setLoading(false)
-             })
-     }, [categoriaId])
+    //   data
+    //     .then((res) => {
+    //        if(categoriaId){
+    //          setItems(res.filter((item)=>item.category === categoriaId))
+    //    } else {
+    //      setItems(res)
+    //}
+    //})
+    //.catch((err) => {
+    //   setError(err)
+    //})
+    //.finally(() => {
+    //  setLoading(false)
+    //})
+    //}, [categoriaId])
 
 
     return (
-        <>
-            {/* <div style={{ backgroundColor: bg, padding: pd }}>
-                <h2>El mensaje Greeting es: {greeting}</h2>
-            </div> */}
-            <div>
-                {/* <h2>ItemList</h2> */}
-                <div>{loading ? 'cargando...' : <ItemList items={items} />}</div>
-                <h2> {error ? error : false}</h2>
-                
-            </div>
-        </>
-    )
-}
+        <div>
+            <ItemList items={items} />
+        </div>
+
+    );
+};
+
+export default ItemListContainer;
+
